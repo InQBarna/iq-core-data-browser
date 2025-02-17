ee85e5ad1093fe6764892004c4b1eabbe09f2e21
Jordi Ribalta <ribi_ribi@hotmail.com>
2025-02-17 12:29:11 +0100

    Remove header

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "IQCoreDataBrowser",
-            publicHeadersPath: "include" // Aquest ha de ser dins del target
+            path: "Sources/IQCoreDataBrowser"
         ),
     ]
 )
