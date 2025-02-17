2eb190a0106a6db0aa69b4a8ce23a6d84cc59ed2
Jordi Ribalta <ribi_ribi@hotmail.com>
2025-02-17 13:01:22 +0100

    Fix

--- Package.swift
+++ Package.swift
@@ -6,17 +6,15 @@ import PackageDescription
 let package = Package(
     name: "iq-core-data-browser",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IQCoreDataBrowser",
             targets: ["IQCoreDataBrowser"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "IQCoreDataBrowser",
-            path: "Sources/IQCoreDataBrowser"
+            path: "Sources/IQCoreDataBrowser" // No és necessari especificar publicHeadersPath
         ),
     ]
 )
+
