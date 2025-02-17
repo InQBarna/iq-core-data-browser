f6903e8d52f415237ec3f72846701e99757322e0
Jordi Ribalta <ribi_ribi@hotmail.com>
2025-02-17 12:09:27 +0100

    Create SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iq-core-data-browser",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "IQCoreDataBrowser",
+            targets: ["IQCoreDataBrowser"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "IQCoreDataBrowser"),
+            publicHeadersPath: "include",
+    ]
+)
